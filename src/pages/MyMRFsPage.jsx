da3dfe3191cefbf8f5545427a6b7e47e09afa4f5
Patel Bhavika <bhavikapatel4298@gmail.com>
2026-06-16 09:44:15 +0530

    feat(mrf): set level of urgency to default to Medium in select field

--- src/pages/MyMRFsPage.jsx
+++ src/pages/MyMRFsPage.jsx
@@ -1034,9 +1034,9 @@ export default function MyMRFsPage() {
                     <input name="processOwnerName" value={form.processOwnerName} onChange={handleFormChange} placeholder="Reporting manager name" className={inputCls} />
                   </FormField>
                   <FormField label="Level of Urgency">
-                    <select name="levelOfUrgency" value={form.levelOfUrgency} onChange={handleFormChange} className={selectCls}>
-                      <option>High</option>
+                    <select name="levelOfUrgency" value={form.levelOfUrgency || 'Medium'} onChange={handleFormChange} className={selectCls}>
                       <option>Medium</option>
+                      <option>High</option>
                       <option>Low</option>
                     </select>
                   </FormField>
